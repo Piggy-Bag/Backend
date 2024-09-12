b8300024e9925232a729534f64202e2d192a1d52
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-09-12 15:54:32 +0900

    �⭐init : rfirt commit

---
+++ src/main/java/oink/com/example/PiggyBag/PiggyBagApplication.java
@@ -0,0 +1,13 @@
+package oink.com.example.PiggyBag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyBagApplication.class, args);
+	}
+
+}
